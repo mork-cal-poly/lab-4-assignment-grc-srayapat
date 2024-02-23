67bf85e4701db5bd8afd246b3e38f1ad2d4375e5
Saisri Rayapati <srayapat@calpoly.edu>
2024-02-23 08:22:05 -0800

    sketch.js: 2 figures added with background
    assets: images imported for sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,12 +1,43 @@
+let planet;
+let rocket;
+
+function preload() {
+  planet = loadImage("assets/planet.png");
+  rocket = loadImage("assets/rocket.png");
+}
+
 function setup() {
-  // These lines are fitting our canvas
-  // where we want in the DOM
-  // so that we can manipulate its style
-  // easier
-  let myCanvas = createCanvas(400, 400);
-  myCanvas.parent("canvas-parent");
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  push();
+  translate(200,400);
+  drawBackground(200,400)
+  scale(0.25)
+  drawRocket(-175,-500);
+  drawPlanet(-100,-900)
+  pop();
+}
+
+function drawBackground() {
+  background(10, 30, 50); // Dark blue background
+  fill(255)
+  circle()
+  }
+function drawPlanet(x, y) {
+  push();
+  translate(x, y);
+  scale(0.75);
+  image(planet, -400,-400);
+  pop();
+}
+
+function drawRocket(x, y) {
+  push();
+  translate(x, y);
+  scale(0.25);
+  image(rocket, 0, 0);
+  pop();
 }
